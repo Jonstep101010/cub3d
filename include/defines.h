4e4e2f7f2b62dbef9f4377d98cbbeb9ca21f75eb
Caglar <caglarnal@hotmail.com>
2024-05-15 14:50:35 +0200

    Refactor collision threshold constant

--- include/defines.h
+++ include/defines.h
@@ -8,8 +8,8 @@
 # define MOVE_SPEED (double)0.1
 # define SCREEN_MIDDLE (HEIGHT / 2)
 # define TEXTURE_DEPTH_FACTOR 4
-#define HALF_HEIGHT(height) ((height) / 2)
+# define HALF_HEIGHT(height) ((height) / 2)
 # define BPP sizeof(int32_t)
-#define COLLISION_THRESHOLD 0.3 // Distance threshold for collision detection
+# define COLLISION_THRESHOLD 0.3 // Distance threshold for collision detection
 
 #endif
