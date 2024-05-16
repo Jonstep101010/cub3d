d4d5b1f87d7a52beb99fdc7440f331efb87e96d2
jschwabe <mail@jschwabe.site>
2024-05-16 14:18:17 +0200

    change defines for wall rendering

--- include/defines.h
+++ include/defines.h
@@ -4,9 +4,9 @@
 //aspect ratio 1.32
 # define WIDTH 1320
 # define HEIGHT 1000
-# define ROTATION_SPEED 0.0174
-# define MOVE_SPEED 0.1
+# define ROTATION_SPEED 0.0349
+# define MOVE_SPEED 0.01
 # define TEXTURE_DEPTH_FACTOR 4
-# define COLLISION_THRESHOLD 0.3 // Distance threshold for collision detection
+# define COLLISION_THRESHOLD 2.8026e-45 // Distance threshold for collision detection
 
 #endif
