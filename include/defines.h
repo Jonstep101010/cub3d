e8564dabbea306e9528b10b066ab3c276323324c
jschwabe <mail@jschwabe.site>
2024-05-16 17:43:22 +0200

    fix segfault

--- include/defines.h
+++ include/defines.h
@@ -7,6 +7,6 @@
 # define ROTATION_SPEED 0.0349
 # define MOVE_SPEED 0.1
 # define TEXTURE_DEPTH_FACTOR 4
-# define COLLISION_THRESHOLD 2.8026e-45 // Distance threshold for collision detection
+# define COLLISION_THRESHOLD 0.1 // Distance threshold for collision detection
 
 #endif
