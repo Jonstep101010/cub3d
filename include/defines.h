784a9a88961cafcd8f5971b8edad1c0022e9001c
jschwabe <mail@jschwabe.site>
2024-05-16 14:55:53 +0200

    fix incorrect speed in defines

--- include/defines.h
+++ include/defines.h
@@ -5,7 +5,7 @@
 # define WIDTH 1320
 # define HEIGHT 1000
 # define ROTATION_SPEED 0.0349
-# define MOVE_SPEED 0.01
+# define MOVE_SPEED 0.1
 # define TEXTURE_DEPTH_FACTOR 4
 # define COLLISION_THRESHOLD 2.8026e-45 // Distance threshold for collision detection
 
